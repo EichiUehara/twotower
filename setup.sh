22bab18618baa59177c1173fc78181946ef19abb
eichi_uehara <eichi.uehara@aflo.one>
2024-08-07 02:07:16 +0900

    setup file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
-export $(egrep -v '^#' .env | xargs)
-
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo chown $USER /var/run/docker.sock
-
+# read .env file
+set -o allexport
+[ -f .env ] && . .env
+sudo apt update
+sudo apt install gh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh
+bash miniconda.sh -u
+conda env create -f environment.yml
+git config --global user.email $GITHUB_EMAIL
+git config --global user.name $GITHUB_USER

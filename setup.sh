211227269ac28e1778a3cd64103908b5053ad026
eichi_uehara <eichi.uehara@aflo.one>
2024-08-07 02:10:04 +0900

    setup file

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ sudo apt install gh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -u
 conda env create -f environment.yml
+conda activate twotower
 git config --global user.email $GITHUB_EMAIL
 git config --global user.name $GITHUB_USER

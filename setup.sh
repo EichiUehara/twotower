a468c248ed78f0ac452cbcbc55536d40222f5fd3
eichi_uehara <eichi.uehara@aflo.one>
2024-08-09 06:39:59 +0900

    fix: log store folder

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
 # read .env file
 set -o allexport
 [ -f .env ] && . .env
-sudo apt update
-sudo apt install gh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -u
 source ~/.bashrc
 conda env create -f environment.yml
 conda activate twotower
-git config --global user.email $GITHUB_EMAIL
-git config --global user.name $GITHUB_USER

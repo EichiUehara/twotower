f5e0d24d59923dbc400fa5ff7bf9e897eb0808f0
eichi_uehara <eichi.uehara@aflo.one>
2024-08-09 07:37:05 +0900

    fix: dev config

--- setup.sh
+++
@@ -1,8 +0,0 @@
-# read .env file
-set -o allexport
-[ -f .env ] && . .env
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -u
-source ~/.bashrc
-conda env create -f environment.yml
-conda activate twotower

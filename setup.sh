8b22f0d74300ef1fcf6cb3f1b958b73ad47f0d53
eichi_uehara <eichi.uehara@aflo.one>
2024-08-09 05:30:06 +0900

    fix: setup script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sudo apt update
 sudo apt install gh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -u
+source ~/.bashrc
 conda env create -f environment.yml
 conda activate twotower
 git config --global user.email $GITHUB_EMAIL

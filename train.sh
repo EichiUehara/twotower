364d95c045c0089cfc8c2c4398d67922d2ab8065
eichi_uehara <eichi.uehara@aflo.one>
2024-08-08 15:40:30 +0900

    fix: featureenbeddinglayer

---
+++ train.sh
@@ -0,0 +1,6 @@
+python -m train_amazon_review_ids_midium &&
+python -m train_amazon_review_ids_small &&
+python -m train_amazon_review_text_item_small &&
+python -m train_amazon_review_text_small &&
+python -m train_amazon_review_text_user_small &&
+python -m train_movie_lens

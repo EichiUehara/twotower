87c10eda6c9ff69c864e9dd38c07fe1dccd15541
eichi_uehara <eichi.uehara@aflo.one>
2024-08-10 11:00:11 +0900

    improve ffn

--- layer/FeedForwardNetwork.py
+++ layer/FeedForwardNetwork.py
@@ -4,17 +4,25 @@ from torch.nn import functional as F
 class FeedForwardNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(FeedForwardNetwork, self).__init__()
+        hidden_dim = hidden_dim
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim *2)
-        self.fc3 = nn.Linear(hidden_dim *2, hidden_dim)
-        self.fc4 = nn.Linear(hidden_dim, output_dim)
-        self.dropout = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc5 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc6 = nn.Linear(hidden_dim, output_dim)
+        self.dropout_input_layer = nn.Dropout(0.8)
+        self.dropout = nn.Dropout(0.5)
     def forward(self, x):
+        x = self.dropout_input_layer(x)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = F.relu(self.fc2(x))
         x = self.dropout(x)
         x = F.relu(self.fc3(x))
         x = self.dropout(x)
-        x = self.fc4(x)
+        x = F.relu(self.fc4(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc5(x))
+        x = self.fc6(x)
         return x

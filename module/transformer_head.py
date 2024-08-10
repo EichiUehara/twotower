bdf32d1d0350c6f68e1edb160bbbd682d466c9c4
eichi_uehara <eichi.uehara@aflo.one>
2024-08-10 22:28:09 +0900

    fix: calc transform_head to module

--- module/transformer_head.py
+++ module/transformer_head.py
@@ -1,5 +1,5 @@
 import math
-from module import embedding_dim
+from module.embedding_dim import embedding_dim
 
 def transformer_head(num_classes):
     return math.ceil(math.log2(math.sqrt(embedding_dim(num_classes))))
